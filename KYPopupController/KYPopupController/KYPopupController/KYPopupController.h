6d2e20299c0ec78fbc13b5f4625a31848ff3bb06
kyleYang <yangzychina@gmail.com>
2015-07-09 00:12:27 +0800

    pod init and demo init

---
+++ KYPopupController/KYPopupController/KYPopupController/KYPopupController.h
@@ -0,0 +1,13 @@
+//
+//  KYPopupController.h
+//  KYPopupController
+//
+//  Created by Kyle on 15/7/8.
+//  Copyright (c) 2015年 xiaoluuu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KYPopupController : NSObject
+
+@end
---
+++ KYPopupController/KYPopupController/KYPopupController/KYPopupController.m
@@ -0,0 +1,13 @@
+//
+//  KYPopupController.m
+//  KYPopupController
+//
+//  Created by Kyle on 15/7/8.
+//  Copyright (c) 2015年 xiaoluuu. All rights reserved.
+//
+
+#import "KYPopupController.h"
+
+@implementation KYPopupController
+
+@end
